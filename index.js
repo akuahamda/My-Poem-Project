31fe1ed299883e84999ee68d70703975c7ddf458
akuahamda <hamdalamohammed80@gmail.com>
2024-12-03 15:31:09 -0800

    api integration

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-function generatePoem(event) { 
-    event.preventDefault();
+function displayPoem(response){ 
     new Typewriter('#poem', {
-        strings: "Perhaps the prolific poet was content with letting her mind journey ",
+        strings: response.data.answer,
         autoStart: true,
         delay: 1,
         cursor: "",
       });
+
+}
+
+
+
+
+function generatePoem(event) { 
+    event.preventDefault();
+
+    let instructionsInput = document.querySelector(".instruction")
+    let apiKey = "8fob310357070bce9t79c94a1a8a4f49";
+    let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+    let context = " Make sure to provide a clear and precise answer,about 3 lines" ;
+   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
+
+
+    axios.get(apiUrl).then(displayPoem);   
       
-    
-    
+   
 
 }
 
